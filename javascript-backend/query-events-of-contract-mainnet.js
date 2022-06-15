7cc59ea3b2ddfb527f959bd9818fa45da2aca497
Jihoon <swoohooh@gmail.com>
2022-06-15 22:28:28 +0900

    Change query-events-of-contract.js

--- javascript-backend/query-events-of-contract-mainnet.js
+++ javascript-backend/query-events-of-contract-mainnet.js
@@ -1,220 +1,55 @@
-const Web3 = require("web3");
-var Tx = require("ethereumjs-tx").Transaction;
-
-const rpcURL = process.env.INFURA_MAINNET_ACCESS_TOKEN;
-const web3 = new Web3(rpcURL);
+import Web3 from "web3";
+import { ethers } from "ethers";
+import {
+  ADDRESS_1,
+  ADDRESS_2,
+  INFURA_PROJECT_ID,
+  INFURA_MAINNET_ENDPOINT,
+  INFURA_ROPSTEN_ENDPOINT,
+  CONTRACT_ABI,
+} from "./constants.js";
 
 const contractAddress = "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07";
 
-const contractABI = [
-  {
-    constant: true,
-    inputs: [],
-    name: "mintingFinished",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "name",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_spender", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "approve",
-    outputs: [],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "totalSupply",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_from", type: "address" },
-      { name: "_to", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "transferFrom",
-    outputs: [],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "decimals",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "unpause",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_to", type: "address" },
-      { name: "_amount", type: "uint256" },
-    ],
-    name: "mint",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "paused",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [{ name: "_owner", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "balance", type: "uint256" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "finishMinting",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [],
-    name: "pause",
-    outputs: [{ name: "", type: "bool" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "owner",
-    outputs: [{ name: "", type: "address" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "symbol",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_to", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "transfer",
-    outputs: [],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_to", type: "address" },
-      { name: "_amount", type: "uint256" },
-      { name: "_releaseTime", type: "uint256" },
-    ],
-    name: "mintTimelocked",
-    outputs: [{ name: "", type: "address" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      { name: "_owner", type: "address" },
-      { name: "_spender", type: "address" },
-    ],
-    name: "allowance",
-    outputs: [{ name: "remaining", type: "uint256" }],
-    payable: false,
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [{ name: "newOwner", type: "address" }],
-    name: "transferOwnership",
-    outputs: [],
-    payable: false,
-    type: "function",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "to", type: "address" },
-      { indexed: false, name: "value", type: "uint256" },
-    ],
-    name: "Mint",
-    type: "event",
-  },
-  { anonymous: false, inputs: [], name: "MintFinished", type: "event" },
-  { anonymous: false, inputs: [], name: "Pause", type: "event" },
-  { anonymous: false, inputs: [], name: "Unpause", type: "event" },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "owner", type: "address" },
-      { indexed: true, name: "spender", type: "address" },
-      { indexed: false, name: "value", type: "uint256" },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "from", type: "address" },
-      { indexed: true, name: "to", type: "address" },
-      { indexed: false, name: "value", type: "uint256" },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-];
+const queryEventsOfContractWeb3 = () => {
+  const rpcURL = INFURA_MAINNET_ENDPOINT;
+  const web3 = new Web3(rpcURL);
 
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+  const contract = new web3.eth.Contract(CONTRACT_ABI, contractAddress);
 
-contract.getPastEvents(
-  "AllEvents",
-  {
-    fromBlock: 14854000,
-    toBlock: "latest",
-  },
-  (err, events) => {
-    console.log("err: ", err);
-    console.log(events);
-  }
-);
+  contract.getPastEvents(
+    "AllEvents",
+    {
+      fromBlock: 14854000,
+      toBlock: "latest",
+    },
+    (err, events) => {
+      console.log("[web3]", events.length);
+      // events.forEach((event) => {
+      //   console.log("[web3]", event["blockNumber"], event["event"]);
+      // });
+      console.log("[web3]", events[0]["blockNumber"], events[0]["event"]);
+      console.log("[web3]", events[10]["blockNumber"], events[10]["event"]);
+    }
+  );
+};
+
+const queryEventsOfContractEthers = () => {
+  const provider = new ethers.providers.InfuraProvider(
+    "mainnet",
+    INFURA_PROJECT_ID
+  );
+  const contract = new ethers.Contract(contractAddress, CONTRACT_ABI, provider);
+
+  contract.queryFilter(14854000, 14854000).then((events) => {
+    console.log("[ethers]", events.length);
+    // events.forEach((event) => {
+    //   console.log("[ethers]", event["blockNumber"], event["event"]);
+    // });
+    console.log("[ethers]", events[0]["blockNumber"], events[0]["event"]);
+    console.log("[ethers]", events[10]["blockNumber"], events[10]["event"]);
+  });
+};
+
+queryEventsOfContractWeb3();
+queryEventsOfContractEthers();
--- javascript-backend/query-events-of-contract.js
+++ javascript-backend/query-events-of-contract.js
@@ -1,143 +1,51 @@
-const Web3 = require("web3");
-var Tx = require("ethereumjs-tx").Transaction;
-
-const rpcURL = process.env.INFURA_ROPSTEN_ACCESS_TOKEN;
-const web3 = new Web3(rpcURL);
+import Web3 from "web3";
+import { ethers } from "ethers";
+import {
+  ADDRESS_1,
+  ADDRESS_2,
+  INFURA_PROJECT_ID,
+  INFURA_MAINNET_ENDPOINT,
+  INFURA_ROPSTEN_ENDPOINT,
+  CONTRACT_ABI,
+} from "./constants.js";
 
 const contractAddress = "0xB02e478f0ec5E8980A510D514f18c13A42Cf3067";
 
-const contractABI = [
-  {
-    constant: true,
-    inputs: [],
-    name: "name",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_spender", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "approve",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "totalSupply",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_from", type: "address" },
-      { name: "_to", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "transferFrom",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "standard",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [{ name: "", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "symbol",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_to", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "transfer",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      { name: "", type: "address" },
-      { name: "", type: "address" },
-    ],
-    name: "allowance",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "_from", type: "address" },
-      { indexed: true, name: "_to", type: "address" },
-      { indexed: false, name: "_value", type: "uint256" },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "_owner", type: "address" },
-      { indexed: true, name: "_spender", type: "address" },
-      { indexed: false, name: "_value", type: "uint256" },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-];
+const queryEventsOfContractWeb3 = () => {
+  const rpcURL = INFURA_ROPSTEN_ENDPOINT;
+  const web3 = new Web3(rpcURL);
 
-const contract = new web3.eth.Contract(contractABI, contractAddress);
+  const contract = new web3.eth.Contract(CONTRACT_ABI, contractAddress);
 
-contract.getPastEvents(
-  "AllEvents",
-  {
-    fromBlock: 0,
-    toBlock: "latest",
-  },
-  (err, events) => {
-    console.log(events);
-  }
-);
+  contract.getPastEvents(
+    "AllEvents",
+    {
+      fromBlock: 0,
+      toBlock: "latest",
+    },
+    (err, events) => {
+      console.log(events.length);
+      events.forEach((event) => {
+        console.log("[web3]", event["blockNumber"], event["event"]);
+      });
+    }
+  );
+};
+
+const queryEventsOfContractEthers = () => {
+  const provider = new ethers.providers.InfuraProvider(
+    "ropsten",
+    INFURA_PROJECT_ID
+  );
+  const contract = new ethers.Contract(contractAddress, CONTRACT_ABI, provider);
+
+  contract.queryFilter(0).then((events) => {
+    console.log(events.length);
+    events.forEach((event) => {
+      console.log("[ethers]", event["blockNumber"], event["event"]);
+    });
+  });
+};
+
+queryEventsOfContractWeb3();
+queryEventsOfContractEthers();
