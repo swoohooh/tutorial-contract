7b72c270c005fb23b81eb140f75bf031cfe212b0
Jihoon <swoohooh@gmail.com>
2022-06-16 16:20:58 +0900

    Change send-token-with-txn.js

--- javascript-backend/send-token-with-txn.js
+++ javascript-backend/send-token-with-txn.js
@@ -1,175 +1,123 @@
-const Web3 = require("web3");
-var Tx = require("ethereumjs-tx").Transaction;
-
-const rpcURL = process.env.INFURA_ROPSTEN_ACCESS_TOKEN;
-const web3 = new Web3(rpcURL);
+import Web3 from "web3";
+import { ContractFactory, ethers } from "ethers";
+import {
+  ADDRESS_1,
+  ADDRESS_2,
+  PRIVATE_KEY_1,
+  PRIVATE_KEY_2,
+  PRIVATE_KEY_STR_1,
+  PRIVATE_KEY_STR_2,
+  INFURA_PROJECT_ID,
+  INFURA_MAINNET_ENDPOINT,
+  INFURA_ROPSTEN_ENDPOINT,
+  CONTRACT_ABI,
+} from "./constants.js";
 
 const contractAddress = "0xB02e478f0ec5E8980A510D514f18c13A42Cf3067";
 
-const contractABI = [
-  {
-    constant: true,
-    inputs: [],
-    name: "name",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_spender", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "approve",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "totalSupply",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_from", type: "address" },
-      { name: "_to", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "transferFrom",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "standard",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [{ name: "", type: "address" }],
-    name: "balanceOf",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [],
-    name: "symbol",
-    outputs: [{ name: "", type: "string" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_to", type: "address" },
-      { name: "_value", type: "uint256" },
-    ],
-    name: "transfer",
-    outputs: [{ name: "success", type: "bool" }],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    constant: true,
-    inputs: [
-      { name: "", type: "address" },
-      { name: "", type: "address" },
-    ],
-    name: "allowance",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "_from", type: "address" },
-      { indexed: true, name: "_to", type: "address" },
-      { indexed: false, name: "_value", type: "uint256" },
-    ],
-    name: "Transfer",
-    type: "event",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: true, name: "_owner", type: "address" },
-      { indexed: true, name: "_spender", type: "address" },
-      { indexed: false, name: "_value", type: "uint256" },
-    ],
-    name: "Approval",
-    type: "event",
-  },
-];
+const sendTokenWeb3 = async () => {
+  const rpcURL = INFURA_ROPSTEN_ENDPOINT;
+  const web3 = new Web3(rpcURL);
+  web3.eth.accounts.wallet.add(PRIVATE_KEY_STR_1);
+  const contract = new web3.eth.Contract(CONTRACT_ABI, contractAddress);
 
-const address1 = process.env.ETH_ADDRESS_1;
-const address2 = process.env.ETH_ADDRESS_2;
-
-const privateKey1 = Buffer.from(process.env.ETH_PRIVATE_KEY_1, "hex");
-const privateKey2 = Buffer.from(process.env.ETH_PRIVATE_KEY_2, "hex");
-
-const contract = new web3.eth.Contract(contractABI, contractAddress);
-
-contract.methods.balanceOf(address1).call((err, balance) => {
-  console.log("[B] ETH test1 contract balance: ", balance);
-});
-
-contract.methods.balanceOf(address2).call((err, balance) => {
-  console.log("[B] ETH test2 contract balance: ", balance);
-});
-
-web3.eth.getTransactionCount(address1, (err, txCount) => {
-  const txObject = {
-    nonce: web3.utils.toHex(txCount),
-    gasLimit: web3.utils.toHex(41000),
-    gasPrice: web3.utils.toHex(web3.utils.toWei("10", "gwei")),
-    to: contractAddress,
-    data: contract.methods.transfer(address2, 1000).encodeABI(),
-  };
-
-  const tx = new Tx(txObject, { chain: "ropsten" });
-  tx.sign(privateKey1);
-
-  const serializedTx = tx.serialize();
-  const raw = "0x" + serializedTx.toString("hex");
-
-  web3.eth.sendSignedTransaction(raw, (err, txHash) => {
-    console.log("err: ", err);
-    console.log("txHash: ", txHash);
+  contract.methods.balanceOf(ADDRESS_1).call((err, balance) => {
+    console.log("[web3][b] ETH test1 contract balance: ", balance);
   });
-});
+  contract.methods.balanceOf(ADDRESS_2).call((err, balance) => {
+    console.log("[web3][b] ETH test2 contract balance: ", balance);
+  });
 
-contract.methods.balanceOf(address1).call((err, balance) => {
-  console.log("[A] ETH test1 contract balance: ", balance);
-});
+  const data = contract.methods.transfer(ADDRESS_2, 1000);
+  const gas = await data.estimateGas({ from: ADDRESS_1 });
+  const gasPrice = await web3.eth.getGasPrice();
 
-contract.methods.balanceOf(address2).call((err, balance) => {
-  console.log("[A] ETH test2 contract balance: ", balance);
-});
+  web3.eth
+    .sendTransaction({
+      from: ADDRESS_1,
+      to: contractAddress,
+      data: data.encodeABI(),
+      gasLimit: gas,
+      gasPrice: gasPrice,
+    })
+    .then((txHash) => {
+      console.log("[web3]", "txHash:", txHash);
+      contract.methods.balanceOf(ADDRESS_1).call((err, balance) => {
+        console.log("[web3][a] ETH test1 contract balance: ", balance);
+      });
+
+      contract.methods.balanceOf(ADDRESS_2).call((err, balance) => {
+        console.log("[web3][a] ETH test2 contract balance: ", balance);
+      });
+    });
+};
+
+const sendTokenWeb3Easy = async () => {
+  const rpcURL = INFURA_ROPSTEN_ENDPOINT;
+  const web3 = new Web3(rpcURL);
+  web3.eth.accounts.wallet.add(PRIVATE_KEY_STR_1);
+  const contract = new web3.eth.Contract(CONTRACT_ABI, contractAddress);
+
+  contract.methods.balanceOf(ADDRESS_1).call((err, balance) => {
+    console.log("[web3][b] ETH test1 contract balance: ", balance);
+  });
+  contract.methods.balanceOf(ADDRESS_2).call((err, balance) => {
+    console.log("[web3][b] ETH test2 contract balance: ", balance);
+  });
+
+  const data = contract.methods.transfer(ADDRESS_2, 1000);
+  const gas = await data.estimateGas({ from: ADDRESS_1 });
+  const gasPrice = await web3.eth.getGasPrice();
+
+  data
+    .send({ from: ADDRESS_1, gasLimit: gas, gasPrice: gasPrice })
+    .then((txHash) => {
+      console.log("[web3]", "txHash:", txHash);
+      contract.methods.balanceOf(ADDRESS_1).call((err, balance) => {
+        console.log("[web3][a] ETH test1 contract balance: ", balance);
+      });
+
+      contract.methods.balanceOf(ADDRESS_2).call((err, balance) => {
+        console.log("[web3][a] ETH test2 contract balance: ", balance);
+      });
+    });
+};
+
+const sendTokenEthers = async () => {
+  const provider = new ethers.providers.InfuraProvider(
+    "ropsten",
+    INFURA_PROJECT_ID
+  );
+  const signer = new ethers.Wallet(PRIVATE_KEY_STR_1, provider);
+  const contract = new ethers.Contract(contractAddress, CONTRACT_ABI, signer);
+
+  contract.balanceOf(ADDRESS_1).then((balance) => {
+    console.log("[ethers][b] ETH test1 contract balance: ", balance);
+  });
+  contract.balanceOf(ADDRESS_2).then((balance) => {
+    console.log("[ethers][b] ETH test2 contract balance: ", balance);
+  });
+
+  const gas = await contract.estimateGas.transfer(ADDRESS_2, 1000);
+  const gasPrice = await provider.getGasPrice();
+
+  contract
+    .transfer(ADDRESS_2, 1000, { gasLimit: gas, gasPrice: gasPrice })
+    .then((tx) => {
+      return tx.wait();
+    })
+    .then((txHash) => {
+      console.log("[ethers]", "txHash:", txHash);
+      contract.balanceOf(ADDRESS_1).then((balance) => {
+        console.log("[ethers][a] ETH test1 contract balance: ", balance);
+      });
+
+      contract.balanceOf(ADDRESS_2).then((balance) => {
+        console.log("[ethers][a] ETH test2 contract balance: ", balance);
+      });
+    });
+};
+
+// sendTokenWeb3();
+// sendTokenWeb3Easy();
+sendTokenEthers();
